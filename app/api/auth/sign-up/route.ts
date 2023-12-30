42e3eb82cca8f3442a750499e06c668a8d978d33
Korex23 <ololokes@gmail.com>
2023-12-30 14:05:54 +0100

    not a commit

--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -1,4 +1,7 @@
-import { connectMongoClient, disConnectMongoClient } from "@/src/BE/DB/conection";
+import {
+  connectMongoClient,
+  disConnectMongoClient,
+} from "@/src/BE/DB/conection";
 import { createUser } from "@/src/BE/DB/queries/auth/query";
 import { Email_Signup_Verification } from "@/src/BE/email-service/resend-config";
 import { passwordHasher } from "@/src/core/lib/hashers";
@@ -6,30 +9,44 @@ import { userModelType } from "@/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-try{  const { email, password, confirmPassword } = await req.json();
+  try {
+    const { email, password, confirmPassword } = await req.json();
 
-  // let the same email password and confirm password checks be done servers side too incase of sneaky users
+    // let the same email password and confirm password checks be done servers side too incase of sneaky users
 
- // for any checks error on the email, password, confirmPassword, return the following line of code and place your error message in the message field
+    // for any checks error on the email, password, confirmPassword, return the following line of code and place your error message in the message field
 
- 
-//   NextResponse.json({message:"",data:null},{status:400})
-const hash = passwordHasher(password)
-await connectMongoClient()
-const user = await createUser(email,hash) as userModelType
-await disConnectMongoClient()
-const status = await Email_Signup_Verification(user.UUID,user.email)
-console.log(status,"email")
-return NextResponse.json({status:"success",message:""},{status:201})
-}catch(err:any){
-console.log(err)
-      // this return would be modified with if else to check for several possible errors and return the appropraite messages and the respective status codes
-  // e g the check logic is to see if an email is already on the db and return a fitting response to users
-  const errorMessage = err.message as string
-  if(errorMessage.includes("duplicate key error") && errorMessage.includes("email")){
-    return NextResponse.json({message:"This email is already registered, try signing in",data:null},{status:400})
+    //   NextResponse.json({message:"",data:null},{status:400})
+    const hash = passwordHasher(password);
+    await connectMongoClient();
+    const user = (await createUser(email, hash)) as userModelType;
+    await disConnectMongoClient();
+    const status = await Email_Signup_Verification(user.UUID, user.email);
+    console.log(status, "email");
+    return NextResponse.json(
+      { status: "success", message: "" },
+      { status: 201 }
+    );
+  } catch (err: any) {
+    console.log(err);
+    // this return would be modified with if else to check for several possible errors and return the appropraite messages and the respective status codes
+    // e g the check logic is to see if an email is already on the db and return a fitting response to users
+    const errorMessage = err.message as string;
+    if (
+      errorMessage.includes("duplicate key error") &&
+      errorMessage.includes("email")
+    ) {
+      return NextResponse.json(
+        {
+          message: "This email is already registered, try signing in",
+          data: null,
+        },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { message: "internal error", data: null },
+      { status: 500 }
+    );
   }
-    return NextResponse.json({message:"internal error",data:null},{status:500})
-}
-
 }
--- src/FE/components/SignInForm.tsx
+++ src/FE/components/SignInForm.tsx
@@ -1,28 +1,28 @@
-'use client'
+"use client";
 import { getCsrfToken, signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
 const SignInForm = () => {
-  const router = useRouter()
+  const router = useRouter();
   const googleSignin = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     await signIn("google", { redirect: false });
   };
 
-  const credentialSignIn=async(e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault()
-    const formdata = new FormData(e.currentTarget)
-    const signinResponse=await signIn("credentials",{
-      email:formdata.get("email"),
-      password:formdata.get("password"),
-      redirect:false
-    })
+  const credentialSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formdata = new FormData(e.currentTarget);
+    const signinResponse = await signIn("credentials", {
+      email: formdata.get("email"),
+      password: formdata.get("password"),
+      redirect: false,
+    });
     console.log(signinResponse);
-    if(signinResponse?.status == 200){
-      router.push("/")
+    if (signinResponse?.status == 200) {
+      router.push("/");
     }
-  }
+  };
   return (
     <form onSubmit={credentialSignIn}>
       <label>
@@ -32,17 +32,17 @@ const SignInForm = () => {
       <br />
       <label>
         Password:
-        <input
-          type="password"
-          name="password"
-          id="password"
-        />
+        <input type="password" name="password" id="password" />
       </label>
       <br />
       <button type="submit">Sign In</button>
-      <button type="button" onClick={googleSignin} className="border p-2 rounded">
-            Sign In with Google
-          </button>
+      <button
+        type="button"
+        onClick={googleSignin}
+        className="border p-2 rounded"
+      >
+        Sign In with Google
+      </button>
     </form>
   );
 };
