6bddbbea276dd80893ce9aae8a7bb7a66809369a
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-04-19 17:17:56 +0100

    97% tags modifications

--- analytics/Google.tsx
+++ analytics/Google.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 
 function GoogleAnalytics() {
   return (
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-QS4R8JWB80" strategy='afterInteractive'>
+
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-CK5FQ4VMSR" strategy='afterInteractive'>
   {`<!-- Google tag (gtag.js) -->
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-QS4R8JWB80');
+  gtag('config', 'G-CK5FQ4VMSR');
 `}
 </Script>
 
