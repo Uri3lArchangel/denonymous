b612a772aa34caa3a562372ef940bd7d43dbd26f
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-04-19 04:28:31 +0100

    97.5% robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots():MetadataRoute.Robots{
+    return{
+        rules:[
+            {
+                userAgent:"*",
+                allow:['/','/auth/*','/images/logo.png','images/delogo.png','/r/'],
+                disallow:['/dashboard','/notifications/','/settings/','/api/','/images/','/auth/new-password/']
+            }
+        ]
+    }
+}
