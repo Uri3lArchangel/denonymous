59cb9b47c96fcc5f073db7853f81e2362ed3afb3
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-04-19 19:50:21 +0100

    97.5% google adsense

---
+++ ads/GoogleAdsense.tsx
@@ -0,0 +1,11 @@
+import Script from 'next/script'
+import React from 'react'
+
+function GoogleAdsense() {
+  return (
+<Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5897237172978966" crossOrigin='anonymous'>
+
+</Script>  )
+}
+
+export default GoogleAdsense
