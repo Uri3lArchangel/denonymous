98f2ab0ea088d472ebf8e4a4f0eff3c0bbf50d09
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-05-04 17:54:25 +0100

    first error conrrection

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -7,7 +7,7 @@ export default function GlobalError({
 }) {
   return (
     <main className="flex flex-col items-center justify-center ">
-    <h2 className="text-center">{error.message.split("|")[1]=="client"?error.message.split("|")[0]:error.digest }</h2>
+    <h2 className="text-center">{error.message}</h2>
     <button
       className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
       onClick={
