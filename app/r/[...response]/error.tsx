e4e13092ad5cc016a4cc9064a1812a07362a92bf
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-05-28 02:01:30 +0100

    error filter

--- app/r/[...response]/error.tsx
+++ app/r/[...response]/error.tsx
@@ -11,8 +11,8 @@ const Error: React.FC<ErrorProps> = ({ error }) => {
   console.log(error.message.split("|")[0])
 
   let display ="An error occured"
-  if(error.message.split("|")[0]=="client"){
-    display=error.message.split("|")[1] 
+  if(error.message.split("|")[1]=="client"){
+    display=error.message.split("|")[0] 
   }
 
   return (
