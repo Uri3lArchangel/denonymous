30c467d81c057dc9f3582c5fd13af8a9a141ab45
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-05-28 01:40:13 +0100

    error filter

--- app/r/[...response]/error.tsx
+++ app/r/[...response]/error.tsx
@@ -11,15 +11,13 @@ const Error: React.FC<ErrorProps> = ({ error, reset }) => {
   // Log the error for debugging purposes
   console.error("Error:", error);
 
-  const errorMessageParts = error.message.split("|");
-  const errorMessage = errorMessageParts[1] === "client" 
-    ? errorMessageParts[0] 
-    : "An Error occurred";
+  const errorMessageParts = error.message.split("|")[0] 
+
 
   return (
     <main className="flex flex-col items-center pt-20 bg-black h-[100vh] text-white">
       <MdWarningAmber size={70} className="text-red-500" />
-      <h2 className="text-center text-2xl sm:text-4xl">{errorMessage}</h2>
+      <h2 className="text-center text-2xl sm:text-4xl">{errorMessageParts}</h2>
       <div className="flex flex-col items-center w-full max-w-[400px] sm:flex-row justify-between">
         <button
           className="my-4 rounded-md px-4 py-2 text-sm text-black gradient_elements_div"
