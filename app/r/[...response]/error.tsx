1fdf9e228ca9ff63a45d2353c5c3a8750ce1963f
Uri3lArchangel <solomonhemigadon@gmail.com>
2024-05-28 01:33:07 +0100

    node env for next auth google

--- app/r/[...response]/error.tsx
+++ app/r/[...response]/error.tsx
@@ -1,36 +1,41 @@
 'use client';
 
-import { MdOutlineRunningWithErrors, MdWarningAmber, MdWifiTetheringErrorRounded } from "react-icons/md";
+import { MdWarningAmber } from "react-icons/md";
 
- 
- 
-export default function Error({
-  error,
-  reset,
-}: {
+interface ErrorProps {
   error: Error & { digest?: string };
   reset: () => void;
-}) {
+}
 
- 
+const Error: React.FC<ErrorProps> = ({ error, reset }) => {
+  // Log the error for debugging purposes
+  console.error("Error:", error);
+
+  const errorMessageParts = error.message.split("|");
+  const errorMessage = errorMessageParts[1] === "client" 
+    ? errorMessageParts[0] 
+    : "An Error occurred";
+
   return (
-    <main className="flex  flex-col items-center pt-20 bg-black h-[100vh] text-white">
+    <main className="flex flex-col items-center pt-20 bg-black h-[100vh] text-white">
       <MdWarningAmber size={70} className="text-red-500" />
-      <h2 className="text-center text-2xl sm:text-4xl ">{error.message.split("|")[1]=="client"?error.message.split("|")[0]:"An Error occured" }</h2>
+      <h2 className="text-center text-2xl sm:text-4xl">{errorMessage}</h2>
       <div className="flex flex-col items-center w-full max-w-[400px] sm:flex-row justify-between">
-      <button
-        className="my-4 rounded-md  px-4 py-2 text-sm text-black gradient_elements_div"
-        onClick={
-          // Attempt to recover by trying to re-render the invoices route
-          () => {window.location.reload()}
-        }
-      >
-        Try again
-      </button>
-      <button onClick={()=>{
-        window.history.back()
-      }}>Go back</button>
+        <button
+          className="my-4 rounded-md px-4 py-2 text-sm text-black gradient_elements_div"
+          onClick={() => window.location.reload()}
+        >
+          Try again
+        </button>
+        <button
+          className="my-4 rounded-md px-4 py-2 text-sm text-black gradient_elements_div"
+          onClick={() => window.history.back()}
+        >
+          Go back
+        </button>
       </div>
     </main>
   );
-}
+};
+
+export default Error;
