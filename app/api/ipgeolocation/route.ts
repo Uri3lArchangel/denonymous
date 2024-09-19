64251bc4e875656cd1bf5edede8a753517ca0b01
urielArchangel <itzdaglitch@gmail.com>
2024-09-19 02:12:36 +0100

    currency and price api

--- app/api/ipgeolocation/route.ts
+++ app/api/ipgeolocation/route.ts
@@ -1,35 +1,61 @@
 import { NextRequest, NextResponse } from "next/server";
-import countryToCurrency, {Countries} from "country-to-currency";
+import countryToCurrency, { Countries } from "country-to-currency";
 import { premiumPriceUSD } from "@/src/core/data/constants";
 
-const getCurrencyFromCountry = (countryCode:Countries) => {
+const getCurrencyFromCountry = (countryCode: Countries) => {
   return countryToCurrency[countryCode]
     ? countryToCurrency[countryCode]
     : "USD";
 };
+const getClientIp = (req: NextRequest) => {
+  const xForwardedFor = req.headers.get("x-forwarded-for");
+  console.log({ xForwardedFor });
+
+  // If the header contains multiple IPs, return the first one
+  if (xForwardedFor) {
+    
+    return xForwardedFor.split(",")[0].trim();
+  }
+
+  // Fallback to X-Real-IP if available
+  console.log({ real:req.headers.get("x-real-ip") });
+
+  return req.headers.get("x-real-ip");
+};
 
 export async function GET(req: NextRequest) {
-  const ip = req.headers.get("x-real-ip") || req.ip;
-  const geoUrl = `https://ipinfo.io/${ip}?token=${process.env.ipinfo_token}`;
-
   try {
-    const res = await fetch(geoUrl);
-    const data = await res.json();
-    console.log({ip,data})
-    const country = data.country;
+    let country;
+    if (!req.geo) {
+      const ip = getClientIp(req);
+      const geoUrl = `https://ipinfo.io/${ip}?token=${process.env.ipinfo_token}`;
+      const res = await fetch(geoUrl);
+      const data = await res.json();
+      country = data.country as string;
+    console.log({ip})
+
+    }
+
+    country = req.geo!.country as Countries;
+    console.log({geo:req.geo})
     const currency = getCurrencyFromCountry(country).toLocaleUpperCase();
     const response = await fetch(
-      `https://api.exchangerate-api.com/v4/latest/${currency}`,{next:{revalidate:3600}}
-    );  
-    
-    const d = await response.json()
-    let baseprice=Number(d.rates[currency]);
-    if(isNaN(baseprice)){
-      baseprice=1;
+      `https://api.exchangerate-api.com/v4/latest/${currency}`,
+      { next: { revalidate: 3600 } }
+    );
+
+    const d = await response.json();
+    let baseprice = Number(d.rates[currency]);
+    if (isNaN(baseprice)) {
+      baseprice = 1;
     }
-    const price =baseprice *premiumPriceUSD
-    console.log({dd:d.rates[currency.toLocaleUpperCase()],cc:currency,country})
-    return NextResponse.json([{price,currency}, null]);
+    const price = baseprice * premiumPriceUSD;
+    console.log({
+      dd: d.rates[currency.toLocaleUpperCase()],
+      cc: currency,
+      country,
+    });
+    return NextResponse.json([{ price, currency }, null]);
   } catch (error: any) {
     return NextResponse.json([null, error.message]);
   }
