4817d90bbf51c302e45c29d7ccc0f22efd39b9dd
urielArchangel <itzdaglitch@gmail.com>
2024-10-08 19:50:39 +0100

    depoints fix

--- app/api/getDePoints/route.ts
+++ app/api/getDePoints/route.ts
@@ -6,21 +6,15 @@ import { u1 } from "@/types";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(req:NextRequest) {
     try {
-        const cookie = cookies().get(cookieKey)
-        if(!cookie||!cookie.value){
-            return NextResponse.json([{points:0,auth:false},null])
-        }
-        const user = verifyUserDataToken(cookie.value)
-        if(!user){
-            return NextResponse.json([{points:0,auth:false},null])
-        }
+        const {username}=await req.json()
         await connectMongo()
-        const u1 = await UserSec.findOne({username:user.username}) as u1
+        const u1 = await UserSec.findOne({username}) as u1
         if(!u1){
             return NextResponse.json([{points:0,auth:false},null])
         }
+        
         return NextResponse.json([{points:u1.points,auth:true},null])
 
     } catch (error:any) {
--- src/BE/components/DePoints.tsx
+++ src/BE/components/DePoints.tsx
@@ -1,12 +1,32 @@
 import DePointsClient from '@/src/FE/components/DePointsClient'
+import { cookieKey } from '@/src/core/data/constants'
+import { verifyUserDataToken } from '@/src/core/lib/JWTFuctions'
+import { cookies } from 'next/headers'
+
+
 
 const DePoints = async() => {
+  let data :{points:number,auth:boolean}
+  const cookie = cookies().get(cookieKey)
+  if(!cookie||!cookie.value){
+      data= {points:0,auth:false}
+  }else{
+  const user = verifyUserDataToken(cookie.value)
+  if(!user){
+    data= {points:0,auth:false}
+  }
+  else{
+  data= {points:0,auth:true}
 
-const res = await fetch(process.env.baseURL!+"/api/getDePoints",{next:{revalidate:false,tags:['denonymous_box_0102','raieneidmie_00','depoints_tag']}})
-const [data,error] = await res.json() as [{points:number,auth:boolean},string]
+const res = await fetch(process.env.baseURL!+"/api/getDePoints",{method:"POST",body:(JSON.stringify({username:user.username})),next:{revalidate:false,tags:['denonymous_box_0102','raieneidmie_00','depoints_tag']}})
+const [d,error] = await res.json() as [{points:number,auth:boolean},string]
 if(error){
 
 }
+data.points=d.points
+}
+
+  }
 
   return (
  <DePointsClient points={data.points} auth={data.auth} />
